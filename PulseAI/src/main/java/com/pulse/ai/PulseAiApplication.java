5059b175a1ccdf278a4ea3b239350e5b7ea8b3b2
Paradox-Adarsh <adarsh27912@gmail.com>
2025-10-15 23:09:58 +0530

    Initial Commit By Adarsh

---
+++ PulseAI/src/main/java/com/pulse/ai/PulseAiApplication.java
@@ -0,0 +1,13 @@
+package com.pulse.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseAiApplication.class, args);
+	}
+
+}
