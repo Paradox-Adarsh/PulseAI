f00b03d706e319ef286380cd5ba99357c09ba111
Paradox-Adarsh <adarsh27912@gmail.com>
2025-10-19 18:42:26 +0530

    Initial commit by Adarsh

---
+++ PulseAI/src/test/java/com/pulse/ai/PulseAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulse.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
