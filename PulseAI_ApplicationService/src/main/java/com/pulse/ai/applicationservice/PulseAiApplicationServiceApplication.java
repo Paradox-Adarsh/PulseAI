05681cad37efde13655c33dbfc4dbe271b7fd813
Paradox-Adarsh <adarsh27912@gmail.com>
2025-10-23 23:06:14 +0530

    Application Service layer for PulseAI, Initial commit by Adarsh

---
+++ PulseAI_ApplicationService/src/main/java/com/pulse/ai/applicationservice/PulseAiApplicationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pulse.ai.applicationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseAiApplicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseAiApplicationServiceApplication.class, args);
+	}
+
+}
---
+++ PulseAI_ApplicationService/src/test/java/com/pulse/ai/applicationservice/PulseAiApplicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulse.ai.applicationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseAiApplicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
