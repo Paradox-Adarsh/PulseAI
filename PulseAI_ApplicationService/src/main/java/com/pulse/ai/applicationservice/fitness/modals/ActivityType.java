e4621ada64629baf1a3f2f045bb93dd29c3db9e6
Paradox-Adarsh <adarsh27912@gmail.com>
2025-10-26 15:34:46 +0530

    created modal classes in application service and added connection to mongodb database successfully, By Adarsh

---
+++ PulseAI_ApplicationService/src/main/java/com/pulse/ai/applicationservice/fitness/modals/ActivityType.java
@@ -0,0 +1,14 @@
+package com.pulse.ai.applicationservice.fitness.modals;
+
+public enum ActivityType {
+	RUNNING,
+	WALKING,
+	SWIMMING,
+	WEIGHT_TRAINING,
+	YOGA,
+	CARDIO,
+	STRECHING,
+	OPTHER
+	
+
+}
