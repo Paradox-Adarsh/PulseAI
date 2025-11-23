0913cf35916661408fe33975c5913e8bc614d6c6
Paradox-Adarsh <adarsh27912@gmail.com>
2025-11-23 16:54:20 +0530

    added log file to track request via multiple services and added ai service for Pulse AI application

--- activity-service/src/main/java/com/pulse/ai/activityservice/fitness/service/UserValidationService.java
+++ activity-service/src/main/java/com/pulse/ai/activityservice/fitness/service/UserValidationService.java
@@ -6,15 +6,17 @@ import org.springframework.web.reactive.function.client.WebClientException;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class UserValidationService {
 
 	private final WebClient userServiceClient;
 
 	public boolean validateUser(String userId) {
-
+		log.info("Calling User Service through webclient");
 		try {
 			return userServiceClient.get().uri("api/users/{userId}/validate", userId).retrieve()
 					.bodyToMono(Boolean.class).block();
