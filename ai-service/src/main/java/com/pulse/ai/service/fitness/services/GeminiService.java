07427a3f48a3410b3d58ff8d72db9763beea16c0
Paradox-Adarsh <adarsh27912@gmail.com>
2026-01-12 23:02:00 +0530

    configured ai service to send message to Gemini Ai model via kafka

---
+++ ai-service/src/main/java/com/pulse/ai/service/fitness/services/GeminiService.java
@@ -0,0 +1,36 @@
+package com.pulse.ai.service.fitness.services;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+public class GeminiService {
+
+	private final WebClient webClient;
+	
+	@Value("${gemini.api.url}")
+	private String geminiApiUrl;
+	
+	@Value("${gemini.api.key}")
+	private String geminiApiKey;
+
+	public GeminiService(WebClient.Builder webClientBuilder) {
+		this.webClient = webClientBuilder.build();
+	}
+
+	public String getRecommendations(String question) {
+		Map<String, Object> requestBody = Map.of("contents",
+				new Object[] { Map.of("parts", new Object[] { Map.of("text", question) }) });
+
+		String response = webClient.post().uri(geminiApiUrl).header("Content-Type", "application/json")
+				.header("x-goog-api-key", geminiApiKey).bodyValue(requestBody).retrieve().bodyToMono(String.class)
+				.block();
+		return response;
+	}
+
+}
