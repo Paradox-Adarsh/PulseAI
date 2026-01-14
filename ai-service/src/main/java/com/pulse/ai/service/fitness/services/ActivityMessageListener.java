d622a175557969ff242d516f2c5212eea657f779
Paradox-Adarsh <adarsh27912@gmail.com>
2026-01-14 14:29:57 +0530

    strctured the response which we would get from gemini ai model

--- ai-service/src/main/java/com/pulse/ai/service/fitness/services/ActivityMessageListener.java
+++ ai-service/src/main/java/com/pulse/ai/service/fitness/services/ActivityMessageListener.java
@@ -5,6 +5,8 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import com.pulse.ai.service.fitness.models.Activity;
+import com.pulse.ai.service.fitness.models.Recommendations;
+import com.pulse.ai.service.fitness.repository.RecommendationRepository;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,11 +18,14 @@ public class ActivityMessageListener {
 	
 	
 	private final ActivityAIService activityAiService;
+	private final RecommendationRepository respository;
 
 	@KafkaListener(topics = "${kafka.topic.name}", groupId = "activity-processor-group")
 	public void processActivity(Activity activity) {
 		log.info("Received activity for processing : {} ", activity.getUserId());
-		activityAiService.generateRecommendation(activity);
+		Recommendations recommendations=activityAiService.generateRecommendation(activity);
+		respository.save(recommendations);
+		
 
 	}
 
